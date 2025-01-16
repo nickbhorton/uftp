0fa3e5191b773e146df8800d16f34881382f7685
Nick Horton <nickkbhorton@gmail.com>
2025-01-16 22:13:04 +0000

    slight refactoring

--- uftp_client.c
+++ uftp_client.c
@@ -7,13 +7,56 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-void useage()
-{
-    printf("usage: uftp_client address port\n  address: the uftp servers "
-           "address\n  port:    the uftp servers port\n");
-}
+void useage();
+
+int validate_address(int argc, char** argv);
+
+int bind_client_socket(int* sockfd_o);
 
 int main(int argc, char** argv)
+{
+    if (validate_address(argc, argv) < 0) {
+        return 1;
+    }
+    int sockfd;
+    if (bind_client_socket(&sockfd) < 0) {
+        return 1;
+    }
+
+    struct addrinfo serv_hints, *serv_res;
+    memset(&serv_hints, 0, sizeof(serv_hints));
+    serv_hints.ai_family = AF_INET;
+    serv_hints.ai_socktype = SOCK_DGRAM;
+
+    int ret;
+    ret = getaddrinfo(argv[1], argv[2], &serv_hints, &serv_res);
+    if (ret != 0) {
+        printf("server getaddrinfo() error: %s\n", gai_strerror(ret));
+        return 1;
+    }
+
+    const char* msg = "hello server";
+    ret = sendto(
+        sockfd,
+        msg,
+        strlen(msg),
+        0,
+        serv_res->ai_addr,
+        serv_res->ai_addrlen
+    );
+    if (ret < 0) {
+        int sendto_err = errno;
+        printf("sendto() error: %s\n", strerror(sendto_err));
+        return sendto_err;
+    } else {
+        printf("sendto() sent: %i bytes\n", ret);
+    }
+
+    shutdown(sockfd, 2);
+    return 0;
+}
+
+int validate_address(int argc, char** argv)
 {
     int ret;
     if (argc != 3) {
@@ -28,24 +71,38 @@ int main(int argc, char** argv)
         ret = inet_pton(AF_INET, argv[1], &(sa.sin_addr));
         if (ret == 0) {
             printf("address given was not a valid network address\n");
-            return 1;
+            return -1;
         } else if (ret < 0) {
             int inet_pton_err = errno;
-            printf("address family is not supported\n");
-            return inet_pton_err;
+            printf(
+                "address family is not supported, %s\n",
+                strerror(inet_pton_err)
+            );
+            return -1;
         }
 
         unsigned short int port;
         if (sscanf(argv[2], "%hu", &port) != 1) {
             printf("port given was not a valid integer\n");
-            return 1;
+            return -1;
         }
         if (port < 5000) {
             printf("port given to small, give a port greater than 5000\n");
-            return 1;
+            return -1;
         }
     }
+    return 0;
+}
 
+void useage()
+{
+    printf("usage: uftp_client address port\n  address: the uftp servers "
+           "address\n  port:    the uftp servers port\n");
+}
+
+int bind_client_socket(int* sockfd_o)
+{
+    int ret;
     struct addrinfo cli_hints, *cli_res;
     memset(&cli_hints, 0, sizeof(cli_hints));
     cli_hints.ai_family = AF_INET;
@@ -55,18 +112,7 @@ int main(int argc, char** argv)
     ret = getaddrinfo(NULL, "0", &cli_hints, &cli_res);
     if (ret != 0) {
         printf("client getaddrinfo() error: %s\n", gai_strerror(ret));
-        return 1;
-    }
-
-    struct addrinfo serv_hints, *serv_res;
-    memset(&serv_hints, 0, sizeof(serv_hints));
-    serv_hints.ai_family = AF_INET;
-    serv_hints.ai_socktype = SOCK_DGRAM;
-
-    ret = getaddrinfo(argv[1], argv[2], &serv_hints, &serv_res);
-    if (ret != 0) {
-        printf("server getaddrinfo() error: %s\n", gai_strerror(ret));
-        return 1;
+        return -1;
     }
 
     int sfd =
@@ -74,7 +120,7 @@ int main(int argc, char** argv)
     if (sfd < 0) {
         int sock_err = errno;
         printf("socket() error: %s\n", strerror(sock_err));
-        return sock_err;
+        return -1;
     }
 
     // beej says this will get rid of that 'pesky "Address already in use" error
@@ -83,31 +129,16 @@ int main(int argc, char** argv)
     if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &tru, sizeof(tru)) == -1) {
         int setsockopt_err = errno;
         printf("setsockopt() error: %s\n", strerror(setsockopt_err));
-        return setsockopt_err;
+        return -1;
     }
 
     ret = bind(sfd, cli_res->ai_addr, cli_res->ai_addrlen);
     if (ret < 0) {
         int bind_err = errno;
         printf("bind() error: %s\n", strerror(bind_err));
-        return bind_err;
+        return -1;
     }
 
-    char msg[] = "hello server";
-    ret = sendto(
-        sfd,
-        msg,
-        strlen(msg),
-        0,
-        serv_res->ai_addr,
-        serv_res->ai_addrlen
-    );
-    if (ret < 0) {
-        int sendto_err = errno;
-        printf("sendto() error: %s\n", strerror(sendto_err));
-        return sendto_err;
-    } else {
-        printf("sendto() sent: %i bytes\n", ret);
-    }
+    *sockfd_o = sfd;
     return 0;
 }
--- uftp_server.c
+++ uftp_server.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,68 +8,50 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-void useage()
-{
-    printf("usage: uftp_server port\n  port: the port you want the server to "
-           "listen on\n");
-}
+void useage();
+int validate_port(int argc, char** argv);
+
+void clear_terminal();
+
+int bind_server_socket_any(char* port_arg, int* sockfd_o);
+
+int server_loop(int sockfd);
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        useage();
+
+    if (validate_port(argc, argv) < 0) {
         return 1;
     }
 
-    unsigned short int port;
-    if (sscanf(argv[1], "%hu", &port) != 1) {
-        printf("port given was not a valid integer\n");
-        return 1;
-    }
-    if (port < 5000) {
-        printf("port given to small, give a port greater than 5000\n");
+    int sockfd;
+    if (bind_server_socket_any(argv[1], &sockfd) < 0) {
         return 1;
     }
 
-    struct addrinfo hints, *res;
-    int sfd;
+    clear_terminal();
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    int ret;
-    ret = getaddrinfo(NULL, argv[1], &hints, &res);
-    if (ret != 0) {
-        printf("getaddrinfo() error: %s\n", gai_strerror(ret));
-        return 1;
+    if (server_loop(sockfd) < 0) {
+        shutdown(sockfd, 2);
     }
-    sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sfd < 0) {
-        int sock_err = errno;
-        printf("socket() error: %s\n", strerror(sock_err));
-        return sock_err;
-    }
+    return 0;
+}
 
-    ret = bind(sfd, res->ai_addr, res->ai_addrlen);
-    if (ret < 0) {
-        int bind_err = errno;
-        printf("bind() error: %s\n", strerror(bind_err));
-        return bind_err;
-    }
-
+int server_loop(int sockfd)
+{
     char msg_buffer[256];
     memset(msg_buffer, 0, sizeof(msg_buffer));
 
     struct sockaddr_storage from;
     socklen_t from_len;
+    int bytes_recv;
     while (1) {
         memset(msg_buffer, 0, sizeof(msg_buffer));
         memset(&from, 0, sizeof(from));
         from_len = sizeof(from);
-        ret = recvfrom(
-            sfd,
+
+        bytes_recv = recvfrom(
+            sockfd,
             msg_buffer,
             sizeof(msg_buffer),
             0,
@@ -76,23 +59,86 @@ int main(int argc, char** argv)
             &from_len
         );
 
-        if (ret >= 0) {
+        if (bytes_recv >= 0) {
             if (from.ss_family == AF_INET) {
-                printf("ipv4,");
-                printf(
-                    "%s,",
-                    inet_ntoa(((struct sockaddr_in*)&from)->sin_addr)
-                );
+                printf("%s", inet_ntoa(((struct sockaddr_in*)&from)->sin_addr));
+                printf(":%u", ntohs(((struct sockaddr_in*)&from)->sin_port));
             } else if (from.ss_family == AF_INET6) {
-                printf("ipv6,");
+                printf("error: found AF_INET6 in sockaddr_storage\n");
+                return -1;
             }
-            printf("%i: ", ret);
+
+            printf("(%i): ", bytes_recv);
             printf("%s\n", msg_buffer);
         } else {
             int recvfrom_err = errno;
             printf("recvfrom() error: %s\n", strerror(recvfrom_err));
-            return recvfrom_err;
+            return -1;
         }
     }
+}
+
+int bind_server_socket_any(char* port_str, int* sockfd_o)
+{
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int ret;
+    ret = getaddrinfo(NULL, port_str, &hints, &res);
+    if (ret < 0) {
+        printf("getaddrinfo() error: %s\n", gai_strerror(ret));
+        return -1;
+    }
+
+    int sfd;
+    sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sfd < 0) {
+        int sock_err = errno;
+        printf("socket() error: %s\n", strerror(sock_err));
+        return -1;
+    }
+
+    ret = bind(sfd, res->ai_addr, res->ai_addrlen);
+    if (ret < 0) {
+        int bind_err = errno;
+        printf("bind() error: %s\n", strerror(bind_err));
+        return -1;
+    }
+
+    *sockfd_o = sfd;
     return 0;
+}
+
+int validate_port(int argc, char** argv)
+{
+    if (argc != 2) {
+        useage();
+        return -1;
+    }
+    unsigned short int port;
+    if (sscanf(argv[1], "%hu", &port) != 1) {
+        printf("port given was not a valid integer\n");
+        return -1;
+    }
+    if (port < 5000) {
+        printf("port given to small, give a port greater than 5000\n");
+        return -1;
+    }
+    return 0;
+}
+
+void useage()
+{
+    printf("usage: uftp_server port\n  port: the port you want the server to "
+           "listen on\n");
+}
+
+void clear_terminal()
+{
+    printf("\033[2J");   // clear terminal
+    printf("\033[1;1H"); // cursor to top left
+    fflush(stdout);      // flush these bytes before we do anthing
 }
